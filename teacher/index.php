cd9543bd5a99849301be214a5b3939fb18591309
stodipo <odipo87@gmail.com>
2017-12-15 00:09:42 +0300

    The teachers log in panel

---
+++ teacher/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Teachers panel</title>
+</head>
+<body>
+	<h1>The Teachers Panel</h1><hr>
+	<h2>Log in</h2>
+	<form method="POST" action="">
+		<label>Username: </label>
+		<input type="text" name="username" placeholder="username">
+		<label>Password: </label>
+		<input type="password" name="password" placeholder="password">
+		<input type="submit" value="Log in" name="login">
+		<input type="reset" value="Reset" name="reset">
+	</form>
+<input type="button" value="Register Here" name="register">
+</body>
+</html>
